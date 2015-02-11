dd2568ee5e44ba97a004cedfaacdeb7ae27ba0c2
yecol <xujb1987@gmail.com>
2015-02-11 10:55:24 +0000

    fork from dgrape.

---
+++ src/main/java/ed/inf/grape/interfaces/Query.java
@@ -0,0 +1,9 @@
+package ed.inf.grape.interfaces;
+
+import java.io.Serializable;
+
+public class Query implements Serializable {
+
+	// TODO to be overwrite in subclass.
+	private static final long serialVersionUID = -95796656026667561L;
+}
